2c2d392cab4c615acf057866220a9e1395109007
Jennisa <jennisa200@hotmail.com>
2021-10-24 01:35:01 +0700

    add rabbitmq

--- src/travelplan/travelplan.controller.ts
+++ src/travelplan/travelplan.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Param, Patch, Post } from "@nestjs/common";
+import { EventPattern } from "@nestjs/microservices";
+import { LocationsPayloadDto } from "./locations.dto";
 import { TravelPlanPayloadDto } from "./travelplan.dto";
 import { TravelPlanService } from "./travelplan.service";
 
 @Controller('api/travelplan')
 export class TravelPlanController {
-    constructor(private travelPlanService: TravelPlanService){}
+    constructor(
+        private travelPlanService: TravelPlanService
+    ){
+    }
+    private travelPlanPayloadDto: TravelPlanPayloadDto = {userId:0,locationId:[]}
     @Post('/add')
-    test(@Body() travelPlanPayloadDto: TravelPlanPayloadDto) {
-        return this.travelPlanService.addTravelPlan(travelPlanPayloadDto)
+    saveTravelPlan(@Body() locationsPayloadDto: LocationsPayloadDto) {
+        this.travelPlanPayloadDto.locationId = locationsPayloadDto.locationId
+        return this.travelPlanService.addTravelPlan(this.travelPlanPayloadDto)
+    }
+    
+    @EventPattern('getCurrentUserId')
+    async getUserId(uid: number) {
+        this.travelPlanPayloadDto.userId = uid
+        console.log(uid)
+        this.saveTravelPlan({locationId:[10500,10200]})
     }
 
     @Patch('/update')
@@ -23,5 +37,6 @@ export class TravelPlanController {
     async all() {
         return this.travelPlanService.all();
     }
+  
 
 }
