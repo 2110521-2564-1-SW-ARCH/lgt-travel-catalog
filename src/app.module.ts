115f4b0e993c71c24d972151ed3ce44513c605dc
Jennisa <jennisa200@hotmail.com>
2021-11-23 14:42:35 +0700

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReviewModule } from './review-location/review.module';
 import { TravelPlanModule } from './travelplan/travelplan.module';
 
 @Module({
